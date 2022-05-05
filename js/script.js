cc38ade00e3d32deb08ab9fecf91b15f56376dae
houssam072 <104954137+houssam072@users.noreply.github.com>
2022-05-05 05:31:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let Img = document.querySelector('.img');
+let Container = document.querySelector('.container');
+function phones (phone){
+    Img.src = phone;
+}
+function pg_Color(color){
+    Container.style.background = color;
+}
